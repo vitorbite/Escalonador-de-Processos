7457eb291007e9099df6aaee6d565e4a9fa216cc
Yan2423 <yanmagnum123@gmail.com>
2025-09-06 17:51:57 -0300

    Adicionar o metodo remover processo

--- ListaDeProcesso.java
+++ ListaDeProcesso.java
@@ -13,8 +13,34 @@ public class ListaDeProcesso {
         // Processo no = new Processo();
         if (cabeça == null) {
             cabeça = novo;
+            cauda = novo;
             tamanho++;
             return;
+        }
+    }
+
+     public void Remover() {
+        if (cabeça.recurso_necessario == "DISCO") {
+            scheduler.lista_bloqueados.Adicionar(cabeça);
+            
+        }
+        // Processo atual = cabeça;
+        if (cabeça == null) {
+            System.out.println("Nenhum processo\n");
+            return;
+        }    
+
+        if (cabeça.ciclos_necessarios == 0) {
+            cabeça = cabeça.proximo;
+            tamanho--;
+        }else{
+            // Adicionar(scheduler.lista_bloqueados.cabeça);
+            
+            cabeça.ciclos_necessarios--; // remove 1 ciclo
+            // Precisa mover para o final da lista
+            // Implementar a melhor estrutura de dados depois
+
+            
         }
     }
 }
