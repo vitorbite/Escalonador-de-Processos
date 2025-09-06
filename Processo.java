a96b3201dde01e6a7e6ae8430d41c188f1591a92
vitorbite <vitorbitencourt20@gmail.com>
2025-09-06 16:54:13 -0300

    Atualizando o Construtor da classe Processo

--- Processo.java
+++ Processo.java
@@ -6,8 +6,12 @@ public class Processo {
     String recurso_necessario;
     Processo proximo;
     
-    public Processo(String nome) {
+public Processo(int id, String nome, int prioridade, int ciclos_necessarios, String recurso_necessario) {
         this.nome = nome;
+        this.id = id;
+        this.prioridade = prioridade;
+        this.ciclos_necessarios = ciclos_necessarios;
+        this.recurso_necessario = recurso_necessario;
         this.proximo = null;
     }
 }
