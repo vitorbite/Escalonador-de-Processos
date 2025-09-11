27f5235f768d2f8abf440e267b53ec2b3eb43cef
Yan2423 <yanmagnum123@gmail.com>
2025-09-10 22:40:05 -0300

    Atualizando o metodo remover

--- ListaDeProcessos.java
+++ ListaDeProcessos.java
@@ -10,7 +10,7 @@ public class ListaDeProcessos {
    }
 
     public void Adicionar(Processo novo) {
-        
+
         if (cabeça == null) {
             cabeça = novo;
             cauda = novo;
@@ -24,26 +24,31 @@ public class ListaDeProcessos {
     
     // Fazer alterações no futuro ------------------------
      public void Remover(ListaDeProcessos lista_bloqueados) {
-        if (cabeça.recurso_necessario == "DISCO") {
-            
-        }
-        // Processo atual = cabeça;
         if (cabeça == null) {
             System.out.println("Nenhum processo\n");
             return;
         }    
-
+        if (cabeça.recurso_necessario == "DISCO") {
+            Processo bloqueado = cabeça;
+            cabeça = cabeça.proximo;
+            bloqueado.proximo = null;
+            tamanho--;
+            lista_bloqueados.Adicionar(bloqueado);
+            System.out.println("Processo " + bloqueado.nome + " movido para bloqueados.");
+            return;
+        }
+         System.out.println("Quantidade de ciclos de " + cabeça.nome + " " + cabeça.ciclos_necessarios);
+        cabeça.ciclos_necessarios--; // remove 1 ciclo
         if (cabeça.ciclos_necessarios == 0) {
+            System.out.println("Processo " + cabeça.nome + " concluído.");
             cabeça = cabeça.proximo;
             tamanho--;
-        }else{
-            //Adicionar(scheduler.lista_bloqueados.cabeça);
-            
-            cabeça.ciclos_necessarios--; // remove 1 ciclo
+        } else {
+            Processo atual = cabeça;
+            cabeça = cabeça.proximo;
+            atual.proximo = null; // Limpa o ponteiro!
+            Adicionar(atual);
             // Precisa mover para o final da lista
-            // Implementar a melhor estrutura de dados depois
-
-            //
         }
     }
 }
