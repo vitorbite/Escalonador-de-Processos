15d4ae08fc48698105f2c5896c980b61de647b0f
vitorbite <vitorbitencourt20@gmail.com>
2025-09-15 09:39:47 -0300

    Criando as instâncias do Scheduler e do LerArquivo.java

--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
 public class Main {
-    public static void main (String[] args){
+    public static void main(String[] args) {
+        Scheduler scheduler = new Scheduler();
+        ListaDeProcessos lista_alta = scheduler.lista_alta_prioridade;
+        ListaDeProcessos lista_media = scheduler.lista_media_prioridade;
+        ListaDeProcessos lista_baixa = scheduler.lista_baixa_prioridade;
+        ListaDeProcessos lista_bloqueados = scheduler.lista_bloqueados;
+
+        LerArquivo leitor = new LerArquivo();
         
     }
-
 }
