a1a60630a36b7540a35977a87101f52d217bb7b9
CobaltSnow172 <arthurcarvalhobc1@gmail.com>
2025-09-16 21:58:45 -0300

    Implementando e corrigindo a regra de anti-inanição

--- Scheduler.java
+++ Scheduler.java
@@ -27,19 +27,21 @@ public class Scheduler {
     // aplicando regra de anti-inanição
 
         if (contador_ciclos_alta_prioridade == 5) {
-            lista_media_prioridade.Remover(lista_bloqueados);
-            if (lista_media_prioridade.tamanho == 0) {
-                lista_baixa_prioridade.Remover(lista_bloqueados);
+                System.out.println("Regra de Anti-inanição acionada.");
+                if (!lista_media_prioridade.eVazia()) {
+
+                    lista_media_prioridade.Remover(lista_bloqueados);
+                    lista_media_prioridade.Imprimir();
+
+                } else if (!lista_baixa_prioridade.eVazia()) {
+                    lista_baixa_prioridade.Remover(lista_bloqueados);
+                    lista_baixa_prioridade.Imprimir();
+                }
+                contador_ciclos_alta_prioridade = 0;
+                System.out.println("==========================================");
+
             }
-            contador_ciclos_alta_prioridade = 0;
             
-        }
-            else {
-             lista_alta_prioridade.Remover(lista_bloqueados);
-             contador_ciclos_alta_prioridade++;
-             lista_media_prioridade.Remover(lista_bloqueados);
-             lista_baixa_prioridade.Remover(lista_bloqueados);
-            }
         } 
     }  
 }
