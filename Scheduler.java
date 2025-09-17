560bd1c3f4576bf7d4cabc09dcb37cbda549002c
vitorbite <vitorbitencourt20@gmail.com>
2025-09-16 22:25:53 -0300

    Consertando e implementando a regra de execução padrão do escalonador

--- Scheduler.java
+++ Scheduler.java
@@ -1,26 +1,62 @@
 public class Scheduler {
-    ListaDeProcessos lista_alta_prioridade = new ListaDeProcessos();
-    ListaDeProcessos lista_media_prioridade = new ListaDeProcessos();
-    ListaDeProcessos lista_baixa_prioridade = new ListaDeProcessos();
+    ListaDeProcessos lista_alta_prioridade = new ListaDeProcessos("Alta Prioridade");
+    ListaDeProcessos lista_media_prioridade = new ListaDeProcessos("Média Prioridade");
+    ListaDeProcessos lista_baixa_prioridade = new ListaDeProcessos("Baixa Prioridade");
 
-    ListaDeProcessos lista_bloqueados = new ListaDeProcessos();
+    ListaDeProcessos lista_bloqueados = new ListaDeProcessos("Bloqueados");
     int contador_ciclos_alta_prioridade = 0;
 
     public void executarCicloDeCpu() {
-// aplicando regra de anti-inanição
-        if (contador_ciclos_alta_prioridade == 5) {
-            lista_media_prioridade.Remover(lista_bloqueados);
-            if (lista_media_prioridade.tamanho == 0) {
-                lista_baixa_prioridade.Remover(lista_bloqueados);
+        while (!lista_alta_prioridade.eVazia() || !lista_media_prioridade.eVazia() || !lista_baixa_prioridade.eVazia() || !lista_bloqueados.eVazia()) {
+            if (!lista_bloqueados.eVazia()) {
+                switch (lista_bloqueados.cabeça.prioridade) {
+                    case 1:
+                        lista_bloqueados.DesbloquearProcesso(lista_alta_prioridade);
+                        break;
+                    case 2:
+                        lista_bloqueados.DesbloquearProcesso(lista_media_prioridade);
+                        break;
+                    case 3:
+                        lista_bloqueados.DesbloquearProcesso(lista_baixa_prioridade);
+                        break;
+                }
+                lista_bloqueados.Imprimir();
             }
-            contador_ciclos_alta_prioridade = 0;
-            
+
+            // Regra de Anti-inanição
+            if (contador_ciclos_alta_prioridade == 5) {
+                System.out.println("Regra de Anti-inanição acionada.");
+                if (!lista_media_prioridade.eVazia()) {
+
+                    lista_media_prioridade.Remover(lista_bloqueados);
+                    lista_media_prioridade.Imprimir();
+
+                } else if (!lista_baixa_prioridade.eVazia()) {
+                    lista_baixa_prioridade.Remover(lista_bloqueados);
+                    lista_baixa_prioridade.Imprimir();
+                }
+                contador_ciclos_alta_prioridade = 0;
+                System.out.println("==========================================");
+
+            } else {
+
+                // Regra de execução padrão
+                if (!lista_alta_prioridade.eVazia()) {
+                    boolean executado = lista_alta_prioridade.Remover(lista_bloqueados);
+                    lista_alta_prioridade.Imprimir();
+                    if (executado) {
+                        contador_ciclos_alta_prioridade++;
+                    }
+                    System.out.println(contador_ciclos_alta_prioridade);
+                } else if (!lista_media_prioridade.eVazia()) {
+                    lista_media_prioridade.Remover(lista_bloqueados);
+                    lista_media_prioridade.Imprimir();
+                } else if (!lista_baixa_prioridade.eVazia()) {
+                    lista_baixa_prioridade.Remover(lista_bloqueados);
+                    lista_baixa_prioridade.Imprimir();
+                }
+            }
+            System.out.println("\n");
         }
-            else {
-             lista_alta_prioridade.Remover(lista_bloqueados);
-             contador_ciclos_alta_prioridade++;
-             lista_media_prioridade.Remover(lista_bloqueados);
-             lista_baixa_prioridade.Remover(lista_bloqueados);
-        }
-    }  
+    }
 }
